cb1964121d230f5655e494148f6e766eb7da94f3
Ashley Manese <ashleymanese31@gmail.com>
2021-07-01 10:27:26 -0700

    tested to color color of the eyes

--- naruto.js
+++ naruto.js
@@ -29,8 +29,11 @@ const RIGHT_MIDDLECHEEK_POINT2 = 352;
 const RIGHT_BOTTOMCHEEK_POINT1 = 426;
 const RIGHT_BOTTOMCHEEK_POINT2 = 433;
 
-
-
+const RIGHT_EYE = 385;
+const LEFT_CORNER_EYE = 362
+const RIGHT_CORNER_EYE = 263
+const TOP_RIGHT_EYE = 386
+const BOTTOM_RIGHT_EYE = 374
 
 function preload() {
     narutoImage = loadImage("assets/naruto_headband.png");
@@ -51,7 +54,7 @@ function setup() {
         latestPrediction = results[0]
         
     })
-    strokeWeight(4)
+    
     
 
     
@@ -90,24 +93,49 @@ function draw() {
 
     let lipWidth = dist(upperLipLocation[0],upperLipLocation[1],bottomLipLocation[0],bottomLipLocation[1])
     //console.log(lipWidth)
-
-  
     
+    // let leftCornerEyeLocation = latestPrediction.scaledMesh[LEFT_CORNER_EYE]
+    // let rightCornerEyeLocation = latestPrediction.scaledMesh[RIGHT_CORNER_EYE]
+    // let distanceEyeWidth = dist(leftCornerEyeLocation[0],leftCornerEyeLocation[1],rightCornerEyeLocation[0], rightCornerEyeLocation[1])
+
+    // let topRightEyeLocation = latestPrediction.scaledMesh[TOP_RIGHT_EYE]
+    // let bottomRightEyeLocation = latestPrediction.scaledMesh[BOTTOM_RIGHT_EYE]
+    // let distanceEyeHeight= dist(topRightEyeLocation[0],topRightEyeLocation[1],bottomRightEyeLocation[0], bottomRightEyeLocation[1])
+    // console.log(distanceEyeHeight)
+
+    // let rightEyeLocation = latestPrediction.scaledMesh[RIGHT_EYE];
+    // noStroke();
+    // fill(255, 144, 33,150);
+    // ellipse(rightEyeLocation[0] + 2, rightEyeLocation[1] + 5,distanceEyeWidth,distanceEyeHeight);
+
+    // circle(rightEyeLocation[0] + 2, rightEyeLocation[1] + 5,20);
+    // noStroke();
+    // fill(255, 144, 33,100);
     //event
     if(lipWidth > 15){
+    
+        
+
+      strokeWeight(4)
+      whisker(LEFT_UPPERCHEEK_POINT1,LEFT_UPPERCHEEK_POINT2);
+      whisker(LEFT_MIDDLECHEEK_POINT1, LEFT_MIDDLECHEEK_POINT2);
+      whisker(LEFT_BOTTOMCHEEK_POINT1, LEFT_BOTTOMCHEEK_POINT2);
+      whisker(RIGHT_UPPERCHEEK_POINT1, RIGHT_UPPERCHEEK_POINT2);
+      whisker(RIGHT_MIDDLECHEEK_POINT1,RIGHT_MIDDLECHEEK_POINT2);
+      whisker(RIGHT_BOTTOMCHEEK_POINT1, RIGHT_BOTTOMCHEEK_POINT2);
+
+    
       
-     whisker(LEFT_UPPERCHEEK_POINT1,LEFT_UPPERCHEEK_POINT2);
-     whisker(LEFT_MIDDLECHEEK_POINT1, LEFT_MIDDLECHEEK_POINT2);
-     whisker(LEFT_BOTTOMCHEEK_POINT1, LEFT_BOTTOMCHEEK_POINT2);
-     whisker(RIGHT_UPPERCHEEK_POINT1, RIGHT_UPPERCHEEK_POINT2);
-     whisker(RIGHT_MIDDLECHEEK_POINT1,RIGHT_MIDDLECHEEK_POINT2);
-     whisker(RIGHT_BOTTOMCHEEK_POINT1, RIGHT_BOTTOMCHEEK_POINT2);
+    
+
+
       
     }
     
     let narutoWidth = foreheadWidth * 3.2;
     let narutoHeight = (narutoImage.height / narutoImage.width) * narutoWidth;
     imageMode(CENTER);
+    // translate(foreheadLocation[0 /* x */], foreheadLocation[1 /* y */] - narutoHeight / 9)
     image(
       narutoImage,
       foreheadLocation[0 /* x */],
@@ -115,6 +143,8 @@ function draw() {
       narutoWidth /* width */,
       narutoHeight /* height */
     );
+
+    
 
     
   }
@@ -127,6 +157,8 @@ function draw() {
 
 
   }
+
+
 
   
   
